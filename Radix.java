af4c245923002c19e1e3a25b809b4e37930d43af
rengzhengcodes <rzhengcontacts@gmail.com>
2021-01-12 12:54:49 -0500

    Added length and nth algorithm.

--- Radix.java
+++ Radix.java
@@ -1,3 +1,11 @@
 public class Radix {
-	
+
+	public static int nth (int n, int col) {
+		return (n / (int)Math.pow(10, col - 1)) % 10;
+	}
+
+	public static int length (int n) {
+		return (int)Math.log10(n);
+	}
+
 }
